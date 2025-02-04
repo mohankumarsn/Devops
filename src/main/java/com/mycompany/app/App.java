85c14becfae26d0848c9439fd348f2dee4f30bcb
root <root@ip-172-31-4-29.ap-south-1.compute.internal>
2025-02-04 06:57:03 +0000

    maven first commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,10 @@
+package com.mycompany.app;
+
+/**
+ * Hello world this is maven target!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World & this is maven!");
+    }
+}
